9d664d4517ff8386fe2d6ae7e4e0f8bc89798fac
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-08-25 05:15:24 +0300

    mp

--- apps/campaigns/views.py
+++ apps/campaigns/views.py
@@ -1,4 +1,6 @@
+from rest_framework import status
 from rest_framework.generics import CreateAPIView, ListCreateAPIView
+from rest_framework.response import Response
 
 from core.permissions import IsDM, IsDMOrReadOnly
 
@@ -24,6 +26,15 @@ class AddGameToCampaign(CreateAPIView):
     permission_classes = (IsDM,)
 
     def perform_create(self, serializer):
+        data = self.request.data
         campaign = self.get_object()
         user = self.request.user
         serializer.save(dm=user, campaign=campaign)
+
+        # s = data.get('scheduledAt').split()[0].split('-')
+        # if int(s[0]) >= campaign.start_scheduledAt.year:
+        #     if int(s[1]) >= campaign.start_scheduledAt.month:
+        #         if int(s[2]) >= campaign.start_scheduledAt.day:
+        #             serializer.save(dm=user, campaign=campaign)
+        # return Response('Game should be scheduled the same day or later than campaign starts',
+        #                 status=status.HTTP_400_BAD_REQUEST)
