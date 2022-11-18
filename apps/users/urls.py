370b1325e92130567ee3a65ab43606d43285f063
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-11-18 02:26:27 +0200

    attempt at creating upd user

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import UserCreateView, UserListCreateView
+from .views import UserCreateView, UserListCreateView, UserRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('', UserCreateView.as_view()),
-    path('/list', UserListCreateView.as_view())
+    path('/list', UserListCreateView.as_view()),
+    # path('/user', UserRetrieveUpdateDestroyView.as_view()),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework.generics import CreateAPIView, ListCreateAPIView
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.generics import CreateAPIView, GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
 
@@ -14,7 +15,28 @@ class UserCreateView(CreateAPIView):
     permission_classes = (AllowAny,)
 
 
-class UserListCreateView(ListCreateAPIView):
+class UserListCreateView(ListCreateAPIView):  # ?
     serializer_class = UserSerializer
     queryset = UserModel.objects.all()
     permission_classes = (IsAdminUser,)
+
+
+# class UserRetrieveUpdateDestroyView(GenericAPIView):
+#     serializer_class = UserSerializer
+#     queryset = UserModel.objects.all()
+#     permission_classes = (IsAuthenticated,)
+#     lookup_field = None
+#
+#     def get(self, *args, **kwargs):
+#         email = self.request.user
+#         # queryset = UserModel.objects.filter(email=email)
+#         # print(queryset)
+#         return Response(UserSerializer(email).data)
+#
+#     def patch(self, pk=None, *args, **kwargs):
+#         email = self.request.user
+#         user = self.get_object()
+#         print(user)
+#         queryset = UserModel.objects.filter(email=email)
+#         print(queryset)
+#         return Response('patch')
